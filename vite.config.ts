11ebd62b947b8105cb6ffe37b9dc3c69d71335e3
chengchongzhen <chengchongzhen@hexfuture.net>
2023-11-01 14:50:58 +0800

    chore: 新增 unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,20 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    uni(),
     UnoCSS(),
     AutoImport({
       imports: ['vue'],
       dts: './typing/auto-imports.d.ts'
-    }),],
+    }),
+    Components({
+      dirs: ['src/components/'],
+      dts: './typing/components.d.ts',
+    }),
+    uni(),
+  ],
 });
