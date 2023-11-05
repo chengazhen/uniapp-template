48d5518f64864ec9c4b15d232df3efe9a1568aab
ccz <15939054361@163.com>
2023-11-05 09:30:30 +0800

    chore: 修改 Components 插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
-import Components from 'unplugin-vue-components/vite'
+import Components from '@uni-helper/vite-plugin-uni-components'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
