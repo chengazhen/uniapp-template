8f86a058b18bf17e7f42184a0a081e80987fe91f
chengchongzhen <chengchongzhen@hexfuture.net>
2024-01-08 15:57:21 +0800

    chore: eslint 规则修改

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
+const antfu = require('@antfu/eslint-config').default
+
 module.exports = antfu({
   overrides: {
     vue: {
       'vue/component-name-in-template-casing': ['off'],
     },
   },
+  ignores: ['src/uni_modules/**/*'],
 })
